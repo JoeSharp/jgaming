7d0ddf6cedb707ce8ea74c9e16140e04206049a3
Joe Sharp <ratracejoe@hotmail.com>
2024-04-24 00:08:29 +0100

    just starting to build a UI

--- jgaming-service/docker/build-docker.sh
+++
@@ -1,6 +0,0 @@
-pushd ..
-
-./gradlew clean build -x test
-docker build --platform=linux/amd64 -t joesharpcs/jgaming-service .
-
-popd
--- k8s/build-docker.sh
+++ k8s/build-docker.sh
@@ -3,6 +3,15 @@ eval $(minikube -p minikube docker-env)
 minikube image ls --format table
 
 echo "Building the Docker Images"
-pushd ../jgaming-service/docker
-./build-docker.sh
+
+echo "Building the Service"
+pushd ../jgaming-service
+./gradlew clean build -x test
+docker build --platform=linux/amd64 -t joesharpcs/jgaming-service .
 popd
+
+echo "Building the UI"
+pushd ../jgaming-ui
+docker build --platform=linux/amd64 -t joesharpcs/jgaming-ui .
+popd
+
