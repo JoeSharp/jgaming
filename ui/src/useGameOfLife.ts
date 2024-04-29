f5f8edc7f19480e4ac3a28e47e54178b4d282392
Joe Sharp <ratracejoe@hotmail.com>
2024-04-29 21:27:27 +0100

    finally got the front end talking to the backend

--- ui/src/useGameOfLife.ts
+++ ui/src/useGameOfLife.ts
@@ -1,34 +1,27 @@
 import { useState, useCallback } from 'react';
 import { GameOfLife, GameOfLifeApi } from './types';
 
-declare global {
-  interface Window {
-    __backendUrl__: string;
-  }
-}
-
 function useGameOfLife(): GameOfLifeApi {
   const [game, setGame] = useState<GameOfLife | null>(null);
 
-  console.log('WOBBLE', import.meta.env.VITE_API_HOST);
-  console.log('Using Game of Life with Backend', window.__backendUrl__);
+  console.debug('Backend API Host', import.meta.env.VITE_API_HOST);
 
   const createGame = useCallback((name?: string) => {
-    fetch(`${window.__backendUrl__}/gol/create${name ? '/' + name : ''}`, { method: 'POST', mode: 'cors' })
+    fetch(`${import.meta.env.VITE_API_HOST}/gol/create${name ? '/' + name : ''}`, { method: 'POST', mode: 'cors' })
       .then(r => r.json())
       .then(g => setGame(g));
   }, []);
 
   const iterateGame = useCallback(() => {
     if (!game) return;
-    fetch(`${window.__backendUrl__}/gol/iterate/${game.id}`, { method: 'POST', mode: 'cors' })
+    fetch(`${import.meta.env.VITE_API_HOST}/gol/iterate/${game.id}`, { method: 'POST', mode: 'cors' })
       .then(r => r.json())
       .then(g => setGame(g));
   }, [game]);
 
   const deleteGame = useCallback(() => {
     if (!game) return;
-    fetch(`${window.__backendUrl__}/gol/${game.id}`, { method: 'DELETE', mode: 'cors' })
+    fetch(`${import.meta.env.VITE_API_HOST}/gol/${game.id}`, { method: 'DELETE', mode: 'cors' })
       .then(() => setGame(null));
   }, [game]);
 
